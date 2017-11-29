443682acd18e559efbd53956c2f36265a9ef8c64
kidozh <kidozh@gmail.com>
2017-11-29 15:54:24 +0800

    add startapp function

---
+++ tjango/management/command/startapp.py
@@ -0,0 +1,19 @@
+from . import baseCommand
+import sys
+
+class command(baseCommand):
+    command_text = 'startapp'
+    create_type = 'app'
+    help_text = 'type <app_name>, <app_name> should be a string and not contradictory with existing package'
+
+    def __init__(self):
+        pass
+
+    def execute(self):
+        argv = sys.argv[1:]
+        if len(argv) == 2:
+            project_name = argv[1]
+        else:
+            raise ValueError('Only one parameters is needed, but you gave %s'%(argv))
+        self.create_project_or_app(self.create_type,project_name)
+        print('App %s has been successfully created' %(project_name))
